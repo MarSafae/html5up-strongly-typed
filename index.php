fe168905e77de165fb3f3dba558199472f86d6b5
Safae MAROUANI <marouani.safae@gmail.com>
2024-11-21 12:27:05 +0000

    init

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+</head>
+<body>
+<div class="container">
+						<header>
+							<h2>Questions or comments? <strong>Get in touch:</strong></h2>
+						</header>
+						<div class="row">
+							<div class="col-6 col-12-medium">
+								<section>
+									<form method="POST" action="#">
+										<div class="row gtr-50">
+											<div class="col-6 col-12-small">
+												<input name="name" placeholder="Name" type="text" required />
+											</div>
+											<div class="col-6 col-12-small">
+												<input name="email" placeholder="Email" type="text" required/>
+											</div>
+											<div class="col-12">
+												<textarea name="message" placeholder="Message" required></textarea>
+											</div>
+											<div class="col-12">
+												<button class="form-button-submit button icon solid fa-envelope" type="submit" name="save">Send Message</button>
+											</div>
+										</div>
+									</form>
+								</section>
+							</div>
+							<div class="col-6 col-12-medium">
+								<section>
+									<p>Erat lorem ipsum veroeros consequat magna tempus lorem ipsum consequat Phaselamet
+									mollis tortor congue. Sed quis mauris sit amet magna accumsan tristique. Curabitur
+									leo nibh, rutrum eu malesuada.</p>
+									<div class="row">
+										<div class="col-6 col-12-small">
+											<ul class="icons">
+												<li class="icon solid fa-home">
+													1234 Somewhere Road<br />
+													Nashville, TN 00000<br />
+													USA
+												</li>
+												<li class="icon solid fa-phone">
+													(000) 000-0000
+												</li>
+												<li class="icon solid fa-envelope">
+													<a href="#">info@untitled.tld</a>
+												</li>
+											</ul>
+										</div>
+										<div class="col-6 col-12-small">
+											<ul class="icons">
+												<li class="icon brands fa-twitter">
+													<a href="#">@untitled</a>
+												</li>
+												<li class="icon brands fa-instagram">
+													<a href="#">instagram.com/untitled</a>
+												</li>
+												<li class="icon brands fa-dribbble">
+													<a href="#">dribbble.com/untitled</a>
+												</li>
+												<li class="icon brands fa-facebook-f">
+													<a href="#">facebook.com/untitled</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</section>
+							</div>
+						</div>
+					</div>
+					<div id="copyright" class="container">
+						<ul class="links">
+							<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</div>
+                
+</body>
+
+</html>
+<?php
+                // if(isset($_POST["name"])){
+                //     echo 'Bonjour'.$_POST["name"];
+                // }
+                // 
+                $_COOKIE_name="user_name";
+                $_COOKIE_value="Kanasayrus";
+                setcookie($_COOKIE_name,$_COOKIE_value,time()+3600,"/");
+                IF(!isset($_COOKIE))
+                
+                
+                ?>
